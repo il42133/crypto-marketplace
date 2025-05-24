8b15ba48ac8b810ef41cb979d4eb60151db96e6f
il42133 <ianlim42133@yahoo.com>
2025-05-24 22:32:08 +0800

    main.js fix

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import { createPinia } from 'pinia'
 
 import 'vuetify/styles'
-import '../mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
